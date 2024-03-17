7cad5bbf0207b9c3d1a29761011ee759d3ccb83b
macovei-alexandru <maccelmare@gmail.com>
2024-03-17 02:07:28 +0200

    game logic done

--- Dictionar/Pages/GamePage.xaml.cs
+++ Dictionar/Pages/GamePage.xaml.cs
@@ -32,20 +32,80 @@ namespace Dictionar.Pages
 
 		private MainWindow ParentWindow => Window.GetWindow(this) as MainWindow;
 		private Dictionary Dictionary => ParentWindow.Dictionary;
-		private Random Random { get; }
+		private Random RNG { get; }
 
-		/*private Tuple<DictionaryEntry, HintType>[] Entries { get; }
-		private string[] Guesses { get; }*/
+		private const int QUESTION_COUNT = 5;
+
+		private Tuple<DictionaryEntry, HintType>[] Entries { get; }
+		private string[] Guesses { get; }
+		private int QuestionCounter { get; set; }
 
 		public GamePage()
 		{
 			InitializeComponent();
-			Random = new Random();
+
+			RNG = new Random();
+
+			Entries = new Tuple<DictionaryEntry, HintType>[QUESTION_COUNT];
+			Guesses = new string[QUESTION_COUNT];
+		}
+
+		private void GenerateRandomEntries()
+		{
+			for (int i = 0; i < QUESTION_COUNT; i++)
+			{
+				var entry = GenerateUniqueRandomEntry(i);
+
+				if (entry.Image != null
+					&& entry.Image != string.Empty
+					&& entry.Image != DictionaryEntry.DefaultImageString)
+				{
+					if (RNG.Next(0, 2) == 0)
+					{
+						Entries[i] = new Tuple<DictionaryEntry, HintType>(entry, HintType.Image);
+					}
+					else
+					{
+						Entries[i] = new Tuple<DictionaryEntry, HintType>(entry, HintType.Definition);
+					}
+				}
+				else
+				{
+					Entries[i] = new Tuple<DictionaryEntry, HintType>(entry, HintType.Definition);
+				}
+			}
+		}
+
+		private DictionaryEntry GenerateUniqueRandomEntry(int lastCheck)
+		{
+			DictionaryEntry entry = null;
+			bool entryFound = true;
+
+			while (entryFound)
+			{
+				entryFound = false;
+
+				entry = Dictionary.Random();
+				for (int i = 0; i < lastCheck; i++)
+				{
+					if (Entries[i].Item1.Word == entry.Word)
+					{
+						entryFound = true;
+						break;
+					}
+				}
+			}
+
+			return entry;
 		}
 
 		private void Page_Loaded(object sender, RoutedEventArgs e)
 		{
-			GenerateNewHint();
+			GenerateRandomEntries();
+
+			previousQuestionButton.IsEnabled = false;
+			QuestionCounter = 0;
+			SetHint(Entries[QuestionCounter]);
 		}
 
 		private void leaveGameButton_Click(object sender, RoutedEventArgs e)
@@ -57,64 +117,78 @@ namespace Dictionar.Pages
 		{
 			if (e.Key == Key.Enter)
 			{
-				GenerateNewHint();
+				nextQuestionButton_Click(null, null);
+			}
+			else if (e.Key == Key.Tab)
+			{
+				previousQuestionButton_Click(null, null);
+				wordTextBox.Focus();
+				e.Handled = true;
+				wordTextBox.SelectionStart = wordTextBox.Text.Length;
 			}
 		}
 
 		private void previousQuestionButton_Click(object sender, RoutedEventArgs e)
 		{
+			if (QuestionCounter == 0)
+			{
+				return;
+			}
+			else if (QuestionCounter == QUESTION_COUNT - 1)
+			{
+				nextQuestionButton.Content = "Next question";
+			}
+
+			QuestionCounter--;
+			wordTextBox.Text = Guesses[QuestionCounter];
+
+			if (QuestionCounter == 0)
+			{
+				previousQuestionButton.IsEnabled = false;
+			}
+
+			SetHint(Entries[QuestionCounter]);
 		}
 
 		private void nextQuestionButton_Click(object sender, RoutedEventArgs e)
 		{
-		}
+			Guesses[QuestionCounter] = wordTextBox.Text;
 
-		private void GenerateNewHint()
-		{
-			var entry = Dictionary.Random();
-			definitionAnswerTextBox.Text = entry.Definition;
+			QuestionCounter++;
+			wordTextBox.Text = string.Empty;
+			previousQuestionButton.IsEnabled = true;
 
-			try
+			if (QuestionCounter == QUESTION_COUNT)
 			{
-				if (entry.Image == null
-					|| entry.Image == string.Empty
-					|| entry.Image == DictionaryEntry.DefaultImageString)
-				{
-					SetHint(entry, HintType.Definition);
-				}
-				else
-				{
-					if (Random.Next(0, 2) == 0)
-					{
-						SetHint(entry, HintType.Definition);
-					}
-					else
-					{
-						SetHint(entry, HintType.Image);
-					}
-				}
+				// TODO: Show the score
+
+				ParentWindow.SwapPage(Utils.Pages.MainPage);
+				return;
 			}
-			catch (Exception exception)
+
+			SetHint(Entries[QuestionCounter]);
+
+			if (QuestionCounter == QUESTION_COUNT - 1)
 			{
-				throw exception;
+				nextQuestionButton.Content = "Finish";
 			}
 		}
 
-		private void SetHint(DictionaryEntry entry, HintType type)
+		private void SetHint(Tuple<DictionaryEntry, HintType> entry)
 		{
-			if (type == HintType.Definition)
+			if (entry.Item2 == HintType.Definition)
 			{
 				definitionAnswerTextBox.Visibility = Visibility.Visible;
-				definitionAnswerTextBox.Text = entry.Definition;
+				definitionAnswerTextBox.Text = entry.Item1.Definition;
 
 				imageImage.Visibility = Visibility.Hidden;
 			}
-			else if (type == HintType.Image)
+			else if (entry.Item2 == HintType.Image)
 			{
 				definitionAnswerTextBox.Visibility = Visibility.Hidden;
 
 				imageImage.Visibility = Visibility.Visible;
-				imageImage.Source = Utils.GetImageFromBase64(entry.Image);
+				imageImage.Source = Utils.GetImageFromBase64(entry.Item1.Image);
 			}
 		}
 	}
